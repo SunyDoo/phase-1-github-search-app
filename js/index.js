252566dfa857aed0f350a3ed060e9f9971590eca
Sunil <snilavarath@gmail.com>
2021-12-22 20:13:39 -0500

    commit tree ef2ee808bdb6b0120964bec23cb8e3b178e9171c

--- js/index.js
+++ js/index.js
@@ -1,12 +1,31 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const form = document.getElementById('github-form')
-    function addUser(login){
+    function addUser(item){
         let user = document.createElement('li')
-        user.innerHTML = login
+        user.innerHTML = `
+        <h1>${item.login}</h1>
+        <img src = ${item.avatar_url} id=avatar>
+        <a href=${item.html_url}>${item.login} on GitHub</a>
+        `
         const userList = document.getElementById('user-list')
         userList.append(user)
+        document.querySelector('h1').addEventListener('click', ()=>{
+            fetch (`https://api.github.com/users/${item.login}/repos`)
+            .then (res=>res.json())
+            // .then (repos=>console.log(repos))
+            .then (repos=>{repos.forEach(item=>addRepo(item))
+            })
+        function addRepo(item){
+            let repo = document.createElement('li')
+            repo.innerText = item.name
+            const repoList = document.getElementById('repos-list')
+            repoList.append(repo)
+            return console.log(item)
+         }
+    })
 
     }
+    
     form.addEventListener('submit', (e)=>{
         let input = e.target.search.value
         // console.log(input)
@@ -14,13 +33,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
         form.reset()
         fetch (`https://api.github.com/search/users?q=${input}`)
         .then (res=>res.json())
-        // .then (data=>console.log(data))
-        .then (data=>{data.items.forEach(login=>addUser(login))
+        // .then (data=>console.log(data.items))
+        .then (data=>{data.items.forEach(item=>addUser(item))
         })
-
-            // console.log(data.items))
-        // .then(data=>Object.keys(data.items).forEach(console.log(items.login.value)))
-        // .then(data=>Object.keys(data.items).forEach(login=>addUser(login)))
     })
     
 })
